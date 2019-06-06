d0cb384d24635875d45b5d2301ee3d6f37684be5
kannankaisika <kannankaisi@gmail.com>
2019-06-06 15:12:39 +0530

    new

---
+++ main.js
@@ -0,0 +1,216 @@
+<script src="//connect.soundcloud.com/sdk.js"></script>
+<script>
+sc.initialite({
+  client_id:"340f063c670272fac27cfa67bffcafc4"
+});
+
+$(document).redy (function(){
+  sc.stream('/tracks/546801033',function(sound){
+$("#sta1").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto1").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+
+$(document).redy (function(){
+  sc.stream('/tracks/577816632',function(sound){
+$("#sta2").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto2").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+
+$(document).redy (function(){
+  sc.stream('/tracks/290818895',function(sound){
+$("#sta3").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto3").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/225519099',function(sound){
+$("#sta4").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto4").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/354639239',function(sound){
+$("#sta5").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto5").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/26197559',function(sound){
+$("#sta6").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto6").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/134780870',function(sound){
+$("#sta7").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto7").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/571882998',function(sound){
+$("#sta8").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto8").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/346418846',function(sound){
+$("#sta9").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto9").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/313263789',function(sound){
+$("#sta10").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto10").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/500780448',function(sound){
+$("#sta11").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto11").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/311741004',function(sound){
+$("#sta12").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto12").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/500792439',function(sound){
+$("#sta13").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto13").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/500776458',function(sound){
+$("#sta14").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto14").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/316859203',function(sound){
+$("#sta15").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto15").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+
+$(document).redy (function(){
+  sc.stream('/tracks/500780448',function(sound){
+$("#sta16").click(function(e){
+e.preventDefault();
+sound.start();
+})
+$("#sto16").click(function(e) {
+e.preventDefault();
+sound.stop();
+     });
+  });)
+})
+</script>
